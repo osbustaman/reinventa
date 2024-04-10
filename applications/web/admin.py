dbbc39c911a5dce0ed85e181168de48daf40b4d6
osbustaman4 <138696569+osbustaman4@users.noreply.github.com>
2024-04-10 12:48:36 -0400

    se crea modelo para las noticias

--- applications/web/admin.py
+++ applications/web/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from applications.web.models import Banner, SocialMediaNews
+
 # Register your models here.
+
+admin.site.register(Banner)
+admin.site.register(SocialMediaNews)
--- applications/web/models.py
+++ applications/web/models.py
@@ -1,3 +1,60 @@
+from django.contrib.auth.models import User
 from django.db import models
+from model_utils.models import TimeStampedModel
+
 
 # Create your models here.
+
+class Banner(TimeStampedModel):
+    OPTIONS = (
+        ('Y', 'SI'),
+        ('N', 'NO'),
+    )
+
+    ba_id = models.AutoField("Key", primary_key=True)
+    ba_title = models.CharField("Bajada de título", max_length=255)
+    ba_download_title = models.CharField("Bajada de título", max_length=255)
+    ba_logo = models.ImageField("Imagen banner", help_text=" Formatos .jpg|.png|.gif|.jpeg", upload_to='image/logo/', null=True, blank=True)
+    ba_element_order = models.IntegerField("Estado de la solicitud", null=True, blank=True)
+    ba_active = models.CharField("Activo", choices=OPTIONS, max_length=1, default="Y")
+
+    def __int__(self):
+        return self.ba_id
+
+    def __str__(self):
+        return f"{self.ba_title.title()}"
+
+    def save(self, *args, **kwargs):
+        # print "save cto"
+        super(Banner, self).save(*args, **kwargs)
+
+    class Meta:
+        db_table = 'banner'
+        ordering = ['ba_id']
+
+
+
+class SocialMediaNews(TimeStampedModel):
+    OPTIONS = (
+        ('Y', 'SI'),
+        ('N', 'NO'),
+    )
+
+    smn_id = models.AutoField("Key", primary_key=True)
+    smn_name = models.CharField("nombre noticia ", max_length=255)
+    smn_link = models.CharField("link ", max_length=255)
+    smn_image = models.ImageField("Imagen banner", help_text="Formatos .jpg|.png|.gif|.jpeg, es opcional", upload_to='image/logo/', null=True, blank=True)
+    smn_active = models.CharField("Activo", choices=OPTIONS, max_length=1, default="Y")
+
+    def __int__(self):
+        return self.smn_id
+
+    def __str__(self):
+        return f"{self.smn_name.title()}"
+
+    def save(self, *args, **kwargs):
+        super(SocialMediaNews, self).save(*args, **kwargs)
+
+    class Meta:
+        db_table = 'social_media_news'
+        ordering = ['smn_id']
