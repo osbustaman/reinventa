3f7265a26178187cf20dc78e9e7fdcc8fea79f6d
dmentron <oabn.trabajo@gmail.com>
2024-07-23 22:34:49 -0400

    se sube pagina en construccion y nueva pagina

--- applications/web/views.py
+++ applications/web/views.py
@@ -5,13 +5,18 @@ from applications.web.models import Banner, SocialMediaNews
 # Create your views here.
 def news(request):
 
+    is_web = request.GET.get('web', None)
 
-    list_object_banner = Banner.objects.filter(ba_active='Y').order_by('ba_element_order')
+    if is_web:
 
-    list_social_media_news = SocialMediaNews.objects.filter(smn_active='Y').order_by('sm_element_order')
+        list_object_banner = Banner.objects.filter(ba_active='Y').order_by('ba_element_order')
+        list_social_media_news = SocialMediaNews.objects.filter(smn_active='Y').order_by('sm_element_order')
 
-    data = {
-        "list_object_banner": list_object_banner,
-        "list_social_media_news": list_social_media_news,
-    }
-    return render(request, 'web/news.html', data)
+        data = {
+            "list_object_banner": list_object_banner,
+            "list_social_media_news": list_social_media_news,
+        }
+        return render(request, 'web/news.html', data)
+    
+    else:
+        return render(request, 'web/page_in_construction.html')
