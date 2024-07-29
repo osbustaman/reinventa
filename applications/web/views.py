92e0e973b070a612692100df530774351fa1bc50
osbustaman4 <osvaldo.bustamante.n@gmail.com>
2024-07-29 15:50:36 -0400

    Update views.py

--- applications/web/views.py
+++ applications/web/views.py
@@ -10,37 +10,31 @@ from applications.web.models import Applications, Banner, BlockHome, Header, Lis
 # Create your views here.
 def news(request):
 
-    is_web = request.GET.get('web', None)
+    list_object_banner = Banner.objects.filter(ba_active='Y').order_by('ba_element_order')
+    list_social_media_news = SocialMediaNews.objects.filter(smn_active='Y').order_by('sm_element_order')
 
-    if is_web:
+    instance_header = Header.objects.filter(he_active='Y').first()
+    list_items = ListItems.objects.filter(header = instance_header, li_active='Y').order_by('li_order')
 
-        list_object_banner = Banner.objects.filter(ba_active='Y').order_by('ba_element_order')
-        list_social_media_news = SocialMediaNews.objects.filter(smn_active='Y').order_by('sm_element_order')
+    list_block_home = BlockHome.objects.filter(bh_active='Y').order_by('bh_order')
 
-        instance_header = Header.objects.filter(he_active='Y').first()
-        list_items = ListItems.objects.filter(header = instance_header, li_active='Y').order_by('li_order')
+    list_element_block_2 = Plugins.objects.filter(blockHome__bh_type_block=2, plu_active='Y').order_by('plu_order')
+    list_element_block_3 = Plugins.objects.filter(blockHome__bh_type_block=3, plu_active='Y').order_by('plu_order')
 
-        list_block_home = BlockHome.objects.filter(bh_active='Y').order_by('bh_order')
+    list_testimonials = Testimonials.objects.filter(tm_active='Y')
 
-        list_element_block_2 = Plugins.objects.filter(blockHome__bh_type_block=2, plu_active='Y').order_by('plu_order')
-        list_element_block_3 = Plugins.objects.filter(blockHome__bh_type_block=3, plu_active='Y').order_by('plu_order')
+    data = {
+        "list_object_banner": list_object_banner,
+        "list_social_media_news": list_social_media_news,
+        "instance_header": instance_header,
+        "list_items": list_items,
+        "list_block_home": list_block_home,
+        "list_element_block_2": list_element_block_2,
+        "list_element_block_3": list_element_block_3,
+        "list_testimonials": list_testimonials
+    }
 
-        list_testimonials = Testimonials.objects.filter(tm_active='Y')
-
-        data = {
-            "list_object_banner": list_object_banner,
-            "list_social_media_news": list_social_media_news,
-            "instance_header": instance_header,
-            "list_items": list_items,
-            "list_block_home": list_block_home,
-            "list_element_block_2": list_element_block_2,
-            "list_element_block_3": list_element_block_3,
-            "list_testimonials": list_testimonials
-        }
-        return render(request, 'web/news.html', data)
-    
-    else:
-        return render(request, 'web/page_in_construction.html')
+    return render(request, 'web/news.html', data)
     
 
 @csrf_exempt  
